a9fd295d9123ad6bc358ffbf583a9ebbc96e1586
Renzo <77223585@certus.edu.pe>
2023-09-16 18:27:21 -0500

    Nueva versión de backlog

---
+++ Final_backlog.py
@@ -0,0 +1,21 @@
+numero1=float(input("Ingrese Numero 1: "))
+numero2=float(input("Ingrese Numero 2: "))
+
+operacion=input("Ingrese Operacion: ")
+if operacion=="suma":
+    print("")
+    print("El resultado es: ", numero1 + numero2)
+elif operacion=="resta":
+    print("")
+    print("El resultado es: ", numero1 - numero2)
+elif operacion == "multiplicacion":
+    print("")
+    print("El resultado es: ", numero1 * numero2)
+elif operacion=="division":
+    print("")
+    print("El resultado es: ", numero1 / numero2)
+
+
+
+
+
